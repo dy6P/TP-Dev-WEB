1d2690451b9d9b8366c866a7c9c0f58fd7443dcb
dy6P <peronidylan06@gmail.com>
2025-11-23 16:43:08 +0100

    Commit.

--- JavaScript/scripts/evenementPhoto.js
+++ JavaScript/scripts/evenementPhoto.js
@@ -8,10 +8,23 @@ function init() {
 }
 
 function over() {
-    let voiliers = ["INITIATIVES COEUR", "SOLIDAIRES EN PELOTON", "TEAM SNEF - TEAMWORK", "ALLAGRANDE MAPEI"]
+    let voiliers = {
+        "Samantha DAVIES" : { nom: "INITIATIVES COEUR", photo: "photos/INITIATIVES_COEUR.jpg" },
+        "Thibaut VAUCHET-CAMUS" : { nom: "SOLIDAIRES EN PELOTON", photo: "photos/SOLIDAIRES_EN_PELOTON.jpg" },
+        "Justine METTRAUX" : { nom: "TEAM SNEF - TEAMWORK", photo: "photos/TEAM_SNEF-TEAMWORK.png" },
+        "Thomas RUYANT" : { nom: "ALLAGRANDE MAPEI", photo: "photos/ALLAGRANDE_MAPEI.jpg" }
+    };
     let skipper = this.getAttribute("alt");
     let result = document.getElementById("result");
-    result.appendChild(document.createTextNode(skipper + " : " + voiliers[this.getAttribute("id") - 1]));
+    result.appendChild(document.createTextNode("NOM" + " : " + skipper));
+    result.appendChild(document.createElement("br"));
+    result.appendChild(document.createTextNode("VOILIER" + " : " + voiliers[skipper]["nom"]));
+    result.appendChild(document.createElement("br"));
+    let img = document.createElement("img");
+    img.src = voiliers[skipper]["photo"];
+    img.style.height = "200px";
+    img.style.width = "180px";
+    result.appendChild(img);
 }
 
 function out() {
