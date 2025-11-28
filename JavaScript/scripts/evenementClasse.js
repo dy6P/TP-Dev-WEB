2b6e13ddb120289537b4f4910dc81c9d507f8877
22401673 <peronidylan06@gmail.com>
2025-11-28 12:29:20 +0100

    Commit.

--- JavaScript/scripts/evenementClasse.js
+++ JavaScript/scripts/evenementClasse.js
@@ -1,15 +1,33 @@
 window.onload = init;
 
-function init() {
-    ajouterLogos();
-}
+const chaineJSON =
+    '[{"nom" : "Class40", "logo" : "Class40.jpg", "voiliers" : "[{"nom" : "LES INVINCIBLES", "annee" : "2024"}, {"nom" : "SEAFRIGO - SOGESTRAN", "annee" : "2023"}]"},' +
+    '{"nom" : "IMOCA", "logo" : "Imoca.jpg", "voiliers" : "[{"nom" : "CHARAL", "annee" : "2022"}, {"nom" : "ALLAGRANDE MAPEI", "annee" : "2023"}]"},' +
+    '{"nom" : "Ocean Fifty", "logo" : "OceanFifty.png", "voiliers" : "[{"nom" : "WEWISE", "annee" : "2019"}, {"nom" : "SOLIDAIRES EN PELOTON", "annee" : "2020"}]"},' +
+    '{"nom" : "Ultim", "logo" : "Ultim.jpg", "voiliers" : "[{"nom" : "SVR - LAZARTIGUE", "annee" : "2021"}, {"nom" : "SODEBO ULTIM 3", "annee" : "2019"}]"}]';
+let classesVoiliers = JSON.parse(chaineJSON);
 
-function ajouterLogos() {
+function init() {
     let divLogo = document.getElementById("logos");
-    let logos = ["photos/logos/Class40.jpg", "photos/logos/Imoca.jpg", "photos/logos/OceanFifty.png", "photos/logos/Ultim.jpg"];
-    for (let i = 0; i < logos.length; i ++) {
-        let logo = logos[i];
-        logo = document.createElement("img");
+    for (let i = 0; i < classesVoiliers.length; i ++) {
+        let logo = document.createElement("img");
+        logo.src = "photos/logos/" + classesVoiliers[i].logo;
+        logo.alt = classesVoiliers[i].nom;
+        logo.id = i;
+        logo.height = 200;
+        logo.width = 200;
+        logo.onmouseover = over;
         divLogo.appendChild(logo);
     }
+}
+
+
+function over() {
+    let divNomDeClasse = document.getElementById("nomDeClasse");
+    while (divNomDeClasse.firstChild) {
+        divNomDeClasse.removeChild(divNomDeClasse.firstChild);
+    }
+    let indice = this.id;
+    let nomClasse = document.createTextNode(classesVoiliers[indice].nom);
+    divNomDeClasse.appendChild(nomClasse);
 }
